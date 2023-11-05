8df154aea4221a4d4ae5f1dbf9e15d6849cb39c4
Mirko Parente <133377396+mirkoparente@users.noreply.github.com>
2023-11-05 12:17:58 +0100

    Modifica Utente password dimenticata reset password

--- WebApplication1/Controllers/UtentiController.cs
+++ WebApplication1/Controllers/UtentiController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -17,5 +19,54 @@ namespace WebApplication1.Controllers
             var utenti = db.AspNetUsers.Where(m=>m.AspNetRoles.Any(i=>i.Name=="User")) ;
             return View(utenti.ToList());
         }
+
+
+        public ActionResult ModificaUtente()
+        {
+            string userUse = User.Identity.Name;
+            AspNetUsers user = db.AspNetUsers.FirstOrDefault(e => e.UserName == userUse);
+            
+            return View(user);
+
+
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ModificaUtente([Bind(Exclude = "IdUtenti")] AspNetUsers user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index","Manage");
+
+            }
+            return View(user);
+
+        }
+
+        public ActionResult Delete(string id)
+        {
+            AspNetUsers utenti = db.AspNetUsers.Find(id);
+            db.AspNetUsers.Remove(utenti);
+            db.SaveChanges();
+            FormsAuthentication.SignOut();
+            Session.Clear();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
     }
 }
